73ac862d6cbec9c8198fbc486f0288dc9e835f7a
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-05-18 20:04:54 +0200

    Added mongodb

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import skillsRoutes from './routes/skills.js';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.DB_CONNECTION;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect(err => {
+  console.log('connected to db');
+  const collection = client.db('test').collection('devices');
+  // perform actions on the collection object
+  client.close();
+});
 const app = express();
-const cors = require('cors');
-const db = require('./db.json');
+// import db from './db.json';
+// const db = require('./db.json');
 
 const PORT = 8080;
 
 // const db = JSON.stringify(db.json);
 // app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use('/all-skills', skillsRoutes);
 
 //ROUTES
-app.get('/all-skills', (req, res) => {
-  res.send(db);
-});
+// app.get('/v1/all-skills', (req, res) => res.send('db'));
 app.get('/', (req, res) => {
   res.send('Add "/all-skills" to the url');
 });
---
+++ routes/skills.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const router = express.Router();
+
+const skills = [
+  {
+    id: 0,
+    icon: '',
+    name: 'DUMMY',
+    web: 'google.com',
+    stack: 'frontend',
+  },
+  {
+    id: 1,
+    icon: 'https://web-dev-skills-api.herokuapp.com/all-skills/skillIcons/1.svg',
+    name: 'HTML',
+    web: 'google.com',
+    stack: 'frontend',
+  },
+  {
+    id: 2,
+    icon: 'https://web-dev-skills-api.herokuapp.com/all-skills/skillIcons/2.svg',
+    name: 'CSS',
+    web: 'google.com',
+    stack: 'frontend',
+  },
+  {
+    id: 3,
+    icon: 'https://web-dev-skills-api.herokuapp.com/all-skills/skillIcons/3.svg',
+    name: 'JavaScript',
+    web: 'google.com',
+    stack: 'frontend',
+  },
+];
+
+router.get('/', (req, res) => {
+  res.send(skills);
+});
+
+export default router;
